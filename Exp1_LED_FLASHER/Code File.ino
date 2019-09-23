ec3c5efc1af02fceaaa34b33d5a2565ab249984b
Mukund Madhav Sharma <55487059+mukund811@users.noreply.github.com>
2019-09-23 22:39:46 +0530

    Create  Code File.ino

---
+++ Exp1_LED_FLASHER/Code File.ino
@@ -0,0 +1,15 @@
+/*Exp. 1 Design an LED Flasher*/
+
+/*Program:-*/
+void setup()
+{
+  pinMode(12, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(12, HIGH);
+  delay(1000); 
+  digitalWrite(12, LOW);
+  delay(1000);
+}
